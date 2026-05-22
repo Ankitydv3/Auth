cf8202982dd4cb8b05c26d13a6bb173eb44f596e
Ankitydv3 <your-email-used-in-github>
2026-05-22 20:08:08 +0530

    Fix production API URL

--- Client/Frontend/src/utils/axios.js
+++ Client/Frontend/src/utils/axios.js
@@ -1,18 +1,20 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: "https://auth-1-uk6n.onrender.com/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-api.interceptors.request.use((config => {
-    const token = localStorage.getItem('token');
-    if (token) {    
-        config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-})
-);
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default api;
