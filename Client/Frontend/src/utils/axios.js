bd5c89b42ae4f85dccf8bcc4a847452c8c00116c
Ankitydv3 <your-email-used-in-github>
2026-05-23 08:50:20 +0530

    all done

--- Client/Frontend/src/utils/axios.js
+++ Client/Frontend/src/utils/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+console.log("API URL:", import.meta.env.VITE_API_URL);
+
 const api = axios.create({
-  baseURL: "https://auth-1-uk6n.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
